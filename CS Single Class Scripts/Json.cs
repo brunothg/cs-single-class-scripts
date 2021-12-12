2da3da4951b169fc8cb6f0007ac4b06086087d73
brunothg <android.bruns@gmail.com>
2021-12-12 15:59:44 +0100

    json parser

---
+++ CS Single Class Scripts/Json.cs
@@ -0,0 +1,504 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace CS_Single_Class_Scripts
+{
+    public static class Json
+    {
+        public static readonly JNull Null = new JNull();
+
+        public abstract class JValue
+        {
+            /**
+             * Get the value (bool, string, double or Dictonary, Array)
+             */
+            public abstract object Value { get; set; }
+
+            /**
+             * Get as String value
+             */
+            public abstract string AsString();
+
+            /**
+             * Get as floating point number or NaN
+             */
+            public double AsNumber() => (IsNumber()) ? double.Parse(Value.ToString()) : double.NaN;
+
+            /**
+             * Get as boolean value oder false
+             */
+            public bool AsBoolean() => (IsBoolean()) && bool.Parse(Value.ToString());
+
+            public bool IsNumber()
+            {
+                return this is JNumber;
+            }
+
+            public bool IsString()
+            {
+                return this is JString;
+            }
+
+            public bool IsBoolean()
+            {
+                return this is JBoolean;
+            }
+
+            public bool IsNull()
+            {
+                return this is JNull;
+            }
+
+            public bool IsObject()
+            {
+                return this is JObject;
+            }
+
+            public bool IsArray()
+            {
+                return this is JArray;
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (obj == null)
+                {
+                    return false;
+                }
+
+                if (!(obj is JValue jObj))
+                {
+                    return false;
+                }
+
+                if (GetType() != obj.GetType())
+                {
+                    return false;
+                }
+
+                var thisAsString = ToJson();
+                var objAsString = jObj.ToJson();
+                return (thisAsString == null && objAsString == null) ||
+                       (thisAsString != null && objAsString != null && thisAsString.Equals(objAsString));
+            }
+
+            public override int GetHashCode()
+            {
+                var stringRepresentation = AsString();
+                return (stringRepresentation != null) ? stringRepresentation.GetHashCode() : -1;
+            }
+
+            /**
+             * Return the value as JSON-String
+             */
+            public abstract string ToJson();
+        }
+
+        public class JArray : JValue
+        {
+            private List<JValue> _array = new List<JValue>();
+
+            public List<JValue> Array
+            {
+                get => _array;
+                set => _array = value ?? new List<JValue>();
+            }
+
+            public override object Value
+            {
+                get => Array;
+                set => Array = (List<JValue>) value;
+            }
+
+            public JArray(List<JValue> value)
+            {
+                Array = value;
+            }
+
+            public JArray() : this(new List<JValue>())
+            {
+            }
+
+            public override string AsString()
+            {
+                return ToJson();
+            }
+
+            public override string ToJson()
+            {
+                var jsonString = "[";
+                foreach (var entry in Array)
+                {
+                    jsonString += entry.ToJson() + ",";
+                }
+
+                jsonString = jsonString.Substring(0, jsonString.Length - 1) + "]";
+
+                return jsonString;
+            }
+        }
+
+        public class JObject : JValue
+        {
+            private Dictionary<string, JValue> _dictionary;
+
+            public Dictionary<string, JValue> Dictionary
+            {
+                get => _dictionary;
+                set => _dictionary = value ?? new Dictionary<string, JValue>();
+            }
+
+            public override object Value
+            {
+                get => Dictionary;
+                set => Dictionary = (Dictionary<string, JValue>) value;
+            }
+
+            public JObject(Dictionary<string, JValue> value)
+            {
+                Dictionary = value;
+            }
+
+            public JObject() : this(new Dictionary<string, JValue>())
+            {
+            }
+
+            public override string AsString()
+            {
+                return ToJson();
+            }
+
+            public override string ToJson()
+            {
+                var jsonString = "{";
+                foreach (var entry in Dictionary)
+                {
+                    jsonString += "\"" + entry.Key + "\":" + entry.Value.ToJson() + ",";
+                }
+
+                jsonString = jsonString.Substring(0, jsonString.Length - 1) + "}";
+
+                return jsonString;
+            }
+        }
+
+        public class JString : JValue
+        {
+            private string _string;
+
+            public string String
+            {
+                get => _string;
+                set => _string = value ?? "";
+            }
+
+            public override object Value
+            {
+                get => String;
+                set => String = (string) value;
+            }
+
+            public JString(string value)
+            {
+                String = value;
+            }
+
+            public JString() : this("")
+            {
+            }
+
+            public override string AsString()
+            {
+                return String;
+            }
+
+            public override string ToJson()
+            {
+                var json = String
+                    .Replace("\\", "\\\\")
+                    .Replace("\n", "\\n")
+                    .Replace("\r", "\\r")
+                    .Replace("\t", "\\t")
+                    .Replace("\b", "\\b")
+                    .Replace("\f", "\\f")
+                    .Replace("\"", "\\\"");
+                return "\"" + json + "\"";
+            }
+        }
+
+        public class JNumber : JValue
+        {
+            public double Double { get; set; }
+
+            public override object Value
+            {
+                get => Double;
+                set => Double = (double) value;
+            }
+
+            public JNumber(double value)
+            {
+                Double = value;
+            }
+
+            public override string AsString()
+            {
+                return Double.ToString();
+            }
+
+            public override string ToJson()
+            {
+                return Double.ToString(CultureInfo.InvariantCulture);
+            }
+        }
+
+        public class JBoolean : JValue
+        {
+            public JBoolean(bool value)
+            {
+                Bool = value;
+            }
+
+            public bool Bool { get; set; }
+
+            public override object Value
+            {
+                get => Bool;
+                set => Bool = (bool) value;
+            }
+
+            public override string AsString()
+            {
+                return Bool.ToString();
+            }
+
+            public override string ToJson()
+            {
+                return (Bool) ? "true" : "false";
+            }
+        }
+
+        public class JNull : JValue
+        {
+            public override object Value
+            {
+                get => null;
+                set { }
+            }
+
+            public override string AsString()
+            {
+                return "null";
+            }
+
+            public override string ToJson()
+            {
+                return "null";
+            }
+        }
+
+        public static string WriteJson(JValue jsonValue)
+        {
+            return jsonValue.ToJson();
+        }
+
+
+        private static readonly Dictionary<char, string> JStringMods = new Dictionary<char, string>
+        {
+            ['\\'] = "\\",
+            ['t'] = "\t",
+            ['n'] = "\n",
+            ['r'] = "\r",
+            ['b'] = "\b",
+            ['f'] = "\f",
+            ['"'] = "\"",
+        };
+
+        private static readonly List<char> JWhitespace = new List<char>()
+        {
+            ' ', '\n', '\r', '\t'
+        };
+
+        private static readonly List<char> JDividers = new List<char>()
+        {
+            ',', ':'
+        };
+
+        private static readonly Regex JNumberRegex = new Regex("^([+-]?[0-9][0-9]*)([.][0-9]*)?([eE][+-]?[0-9]*)?");
+
+        public static JValue ReadJson(string json, out bool ok)
+        {
+            json = json.Trim();
+
+            JValue rootObject = null;
+            var objectStack = new Stack<JValue>();
+            string propertyName = null;
+            var anyArror = false;
+
+            Action<JValue> addValue = (jValue) =>
+            {
+                var resetPropertyName = true;
+                var currentObject = (objectStack.Count <= 0) ? null : objectStack.Peek();
+                if (currentObject != null)
+                {
+                    if (currentObject is JArray jArray)
+                    {
+                        jArray.Array.Add(jValue);
+                    }
+                    else if (currentObject is JObject jObject && propertyName != null)
+                    {
+                        jObject.Dictionary[propertyName] = jValue;
+                    }
+                    else if (currentObject is JObject && propertyName == null && jValue is JString jString)
+                    {
+                        propertyName = jString.String;
+                        resetPropertyName = false;
+                    }
+                    else
+                    {
+                        anyArror = true;
+                    }
+                }
+
+                if (resetPropertyName)
+                {
+                    propertyName = null;
+                }
+
+                if (jValue is JObject || jValue is JArray)
+                {
+                    objectStack.Push(jValue);
+                }
+
+                if (rootObject == null)
+                {
+                    rootObject = jValue;
+                }
+            };
+
+            Action closeValue = () =>
+            {
+                if (objectStack.Count > 0)
+                {
+                    objectStack.Pop();
+                }
+                else
+                {
+                    anyArror = true;
+                }
+            };
+
+            for (var i = 0; i < json.Length; i++)
+            {
+                if (json[i] == '{')
+                {
+                    addValue(new JObject());
+                }
+                else if (json[i] == '}')
+                {
+                    closeValue();
+                }
+                else if (json[i] == '[')
+                {
+                    addValue(new JArray());
+                }
+                else if (json[i] == ']')
+                {
+                    closeValue();
+                }
+                else if (json[i] == '"')
+                {
+                    var jString = "";
+                    var mask = false;
+                    while (++i < json.Length)
+                    {
+                        if (json[i] == '\\' && !mask)
+                        {
+                            mask = true;
+                        }
+                        else if (json[i] == '"' && !mask)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            if (mask)
+                            {
+                                mask = false;
+                                var maskValue = JStringMods[json[i]];
+                                if (maskValue != null)
+                                {
+                                    jString += maskValue;
+                                }
+                                else
+                                {
+                                    // Unknown mask value
+                                    anyArror = true;
+                                }
+                            }
+                            else
+                            {
+                                jString += json[i];
+                            }
+                        }
+                    }
+
+                    addValue(new JString(jString));
+                }
+                else if (JWhitespace.Contains(json[i]) || JDividers.Contains(json[i]))
+                {
+                    // Can be ignored
+                }
+                else if (json.Substring(i).StartsWith("true"))
+                {
+                    addValue(new JBoolean(true));
+                    i += 3;
+                }
+                else if (json.Substring(i).StartsWith("false"))
+                {
+                    addValue(new JBoolean(false));
+                    i += 4;
+                }
+                else if (json.Substring(i).StartsWith("null"))
+                {
+                    addValue(new JNull());
+                    i += 3;
+                }
+                else
+                {
+                    var numberMatch = JNumberRegex.Match(json.Substring(i));
+                    if (numberMatch.Success && numberMatch.Index == 0)
+                    {
+                        addValue(new JNumber(double.Parse(
+                            numberMatch.Value,
+                            NumberStyles.Float,
+                            CultureInfo.InvariantCulture
+                        )));
+                        i += numberMatch.Length - 1;
+                    }
+                    else
+                    {
+                        // Unknown or unexpected char
+                        anyArror = true;
+                    }
+                }
+            }
+
+            // Unfinished objects
+            if (objectStack.Count > 0)
+            {
+                anyArror = true;
+            }
+
+            ok = !anyArror;
+            return rootObject;
+        }
+
+        public static JValue ReadJson(string json)
+        {
+            return ReadJson(json, out _);
+        }
+    }
+}
---
+++ Tests/JsonTests.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using CS_Single_Class_Scripts;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class JsonTests
+    {
+        private const string JsonString = @"{
+    ""v1"": ""Das ist ein\\\"" string"",
+    ""v2"": true,
+    ""v3"": 123.394,
+    ""v4"": [1,2, 1.5],
+    ""v5"": [
+        {""v51"": false, ""v52"": 1.2},
+        {""v51"": true, ""v52"": 1.3},
+        {""v51"": true, ""v52"": 1.4}
+    ],
+    ""v6"": null
+}";
+
+        private static readonly Json.JValue JsonValue = new Json.JObject(new Dictionary<string, Json.JValue>
+        {
+            ["v1"] = new Json.JString("Das ist ein\\\" string"),
+            ["v2"] = new Json.JBoolean(true),
+            ["v3"] = new Json.JNumber(123.394),
+            ["v4"] = new Json.JArray(new List<Json.JValue>
+                {
+                    new Json.JNumber(1),
+                    new Json.JNumber(2),
+                    new Json.JNumber(1.5)
+                }
+            ),
+            ["v5"] = new Json.JArray(new List<Json.JValue>
+                {
+                    new Json.JObject(new Dictionary<string, Json.JValue>
+                    {
+                        ["v51"] = new Json.JBoolean(false),
+                        ["v52"] = new Json.JNumber(1.2)
+                    }),
+                    new Json.JObject(new Dictionary<string, Json.JValue>
+                    {
+                        ["v51"] = new Json.JBoolean(true),
+                        ["v52"] = new Json.JNumber(1.3)
+                    }),
+                    new Json.JObject(new Dictionary<string, Json.JValue>
+                    {
+                        ["v51"] = new Json.JBoolean(true),
+                        ["v52"] = new Json.JNumber(1.4)
+                    })
+                }
+            ),
+            ["v6"] = Json.Null
+        });
+
+        private static readonly List<string> JsonExamples = new List<string>()
+        {
+            @"[ 100, 500, 300, 200, 400 ]",
+            @"{""color"":""red"",""value"":""#f00""}",
+            @"{""id"":""0001"",""type"":""donut"",""name"":""Cake"",""ppu"":0.55,""batters"":{""batter"":[{""id"":""1001"",""type"":""Regular""},{""id"":""1002"",""type"":""Chocolate""},{""id"":""1003"",""type"":""Blueberry""},{""id"":""1004"",""type"":""Devil's Food""}]},""topping"":[{""id"":""5001"",""type"":""None""},{""id"":""5002"",""type"":""Glazed""},{""id"":""5005"",""type"":""Sugar""},{""id"":""5007"",""type"":""Powdered Sugar""},{""id"":""5006"",""type"":""Chocolate with Sprinkles""},{""id"":""5003"",""type"":""Chocolate""},{""id"":""5004"",""type"":""Maple""}]}",
+            @"[{""id"":""0001"",""type"":""donut"",""name"":""Cake"",""ppu"":0.55,""batters"":{""batter"":[{""id"":""1001"",""type"":""Regular""},{""id"":""1002"",""type"":""Chocolate""},{""id"":""1003"",""type"":""Blueberry""},{""id"":""1004"",""type"":""Devil's Food""}]},""topping"":[{""id"":""5001"",""type"":""None""},{""id"":""5002"",""type"":""Glazed""},{""id"":""5005"",""type"":""Sugar""},{""id"":""5007"",""type"":""Powdered Sugar""},{""id"":""5006"",""type"":""Chocolate with Sprinkles""},{""id"":""5003"",""type"":""Chocolate""},{""id"":""5004"",""type"":""Maple""}]},{""id"":""0002"",""type"":""donut"",""name"":""Raised"",""ppu"":0.55,""batters"":{""batter"":[{""id"":""1001"",""type"":""Regular""}]},""topping"":[{""id"":""5001"",""type"":""None""},{""id"":""5002"",""type"":""Glazed""},{""id"":""5005"",""type"":""Sugar""},{""id"":""5003"",""type"":""Chocolate""},{""id"":""5004"",""type"":""Maple""}]},{""id"":""0003"",""type"":""donut"",""name"":""Old Fashioned"",""ppu"":0.55,""batters"":{""batter"":[{""id"":""1001"",""type"":""Regular""},{""id"":""1002"",""type"":""Chocolate""}]},""topping"":[{""id"":""5001"",""type"":""None""},{""id"":""5002"",""type"":""Glazed""},{""id"":""5003"",""type"":""Chocolate""},{""id"":""5004"",""type"":""Maple""}]}]",
+            @"{""id"":""0001"",""type"":""donut"",""name"":""Cake"",""image"":{""url"":""images/0001.jpg"",""width"":200,""height"":200},""thumbnail"":{""url"":""images/thumbnails/0001.jpg"",""width"":32,""height"":32}}",
+        };
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestReadJson()
+        {
+            var json = Json.ReadJson(JsonString, out var ok);
+            Console.Out.WriteLine(json.AsString());
+            Assert.True(ok);
+            Assert.AreEqual(JsonValue, json);
+        }
+
+        [Test]
+        public void TestWriteJson()
+        {
+            var json = Json.WriteJson(JsonValue);
+            Console.Out.WriteLine(json);
+            Assert.AreEqual(
+                Regex.Replace(JsonString, @"\s+", ""),
+                Regex.Replace(json, @"\s+", "")
+            );
+        }
+
+
+        [Test]
+        public void TestJsonSamples()
+        {
+            foreach (var jsonExample in JsonExamples)
+            {
+                var jValue = Json.ReadJson(jsonExample, out var ok);
+                Assert.True(ok);
+                var json = Json.WriteJson(jValue);
+                Assert.AreEqual(
+                    Regex.Replace(jsonExample, @"\s+", ""),
+                    Regex.Replace(json, @"\s+", "")
+                );
+            }
+        }
+    }
+}
